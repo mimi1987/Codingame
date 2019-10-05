4cfd774f9062ec767d2201894330104c30b3faea
Michael M <m.migsch@gmail.com>
2019-10-05 21:33:22 +0200

    Update temprature.py

--- temprature.py
+++ temprature.py
@@ -5,49 +5,33 @@ import math
 # the standard input according to the problem statement.
 
 n = int(input())  # the number of temperatures to analyse
+pos = []
+neg = []
 
-
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
-
-# 1. sorting the input tempratures into negative and positive values
-positive_tempratures = []
-negative_tempratures = []
 
 for i in input().split():
-    # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
-    
-    if t >= 0:
-        positive_tempratures.append(t)
+    if (t == 0 or t > 0):
+        pos.append(t)
     else:
-        negative_tempratures.append(t)
+        neg.append(t)
 
-# 2. sorting the negative and positive temprature list increasing order
-positive_tempratures.sort()
-negative_tempratures.sort(reverse=True)
+pos.sort()
+neg.sort(reverse = True)
 
-# 3. test for no input values
-if not positive_tempratures and not negative_tempratures:
-    print("0")
+if (pos == [] and neg == []):
+    print(0)
+elif 0 in pos:
+    print(0)
+elif (pos != [] and neg != []):
+        if (pos[0] == abs(neg[0])):
+            print(pos[0])
+        elif (pos[0] < abs(neg[0])):
+              print(pos[0])
+        else:
+              print(neg[0])
+elif (neg != [] and pos == []):
+    print(neg[0])
+elif (neg == [] and pos != []):
+    print(pos[0])
 
-# 4.
-#   Test if a zero is in the positive_tempratures list
-#   compare the two smallest values if the substract to zero, if so output the first value from the positive temp list
-#   compare if they are resulting into a negative number, if so output the first positive temp value
-#   compare if they the values resulting into a positive number, if so output the first negative temp value
-if 0 in positive_tempratures:
-    print("0")
-elif positive_tempratures and negative_tempratures:
-    if positive_tempratures[0] + negative_tempratures[0] == 0:
-        print("{}".format(positive_tempratures[0]))
-    elif positive_tempratures[0] + negative_tempratures[0] < 0:
-        print("{}".format(positive_tempratures[0]))
-    elif positive_tempratures[0] + negative_tempratures[0] > 0:
-        print("{}".format(negative_tempratures[0]))
-    else:
-        print("Don't know yet what went wrong!")
-elif not positive_tempratures and negative_tempratures:
-    print("{}".format(negative_tempratures[0]))
-elif positive_tempratures and not negative_tempratures:
-    print("{}".format(positive_tempratues[0]))
